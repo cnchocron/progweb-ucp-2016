f9ea84e58e6f7e843fcdbba4b031984f4b3abea6
cnchocron <cnchocron@gmail.com>
2016-04-14 19:05:28 -0300

    probando respuestas

--- src/simple-nodejs-azure/cnchocron/app.js
+++ src/simple-nodejs-azure/cnchocron/app.js
@@ -24,6 +24,73 @@
         return miLista;
     }
     
+    self.preguntaArmar = function (id) {
+        
+        
+        
+        var pregunta = {
+            id: 1,
+            descripcion : '¿Cual es la moneda Oficial de Costa Rica?',
+            respuestas : [
+                {
+                    id: 1,
+                    descripcion: 'Peso de Costa Rica'
+                },
+                {
+                    id: 2,
+                    descripcion: 'Dolar de Costa Rica'
+                },
+                {
+                    id: 3,
+                    descripcion: 'Colon de Costa Rica'
+                },
+                {
+                    id: 4,
+                    descripcion: 'Dolar Americano'
+                },
+            ]
+        };
+        
+        var html = '';
+        html += '<div class="row">';
+        
+        //titulo
+        html += '<div name="preguntaID" class="col-md-12 pregunta-titulo">';
+        html += '<h2>';
+        html += pregunta.descripcion;
+        html += '</h2>';
+        html += '</div>'; //fin de pregunta-titulo
+        
+        
+        
+        
+        //respuestas
+        html += '<div class="col-md-offset-3 col-md-6 pregunta-respuestas">';
+        
+        html += '<form action="" method="get">';
+        
+        for (i = 0; i < pregunta.respuestas.length; i++) {
+            var r = pregunta.respuestas[i];
+            html += '';
+            
+            html += '<input name="respuestaID" type="submit" class="btn btn-primary col-md-12 col-xs-12 " value="' + r.descripcion + ' " />';
+
+
+            html += '';
+        }
+        html += '</form>';
+        
+        
+
+        html += '</div>'; //fin de pregunta-respuestas
+        
+        
+        html += '</div>'; //fin de row
+        
+        
+        return html;
+    }
+    
     return self;
 }();
 
